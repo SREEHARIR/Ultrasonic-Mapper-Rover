c0d52b0ab88bb27ec60ae4de22f8133bd8bda795
R SREEHARI <sreehari.r96@gmail.com>
2015-10-02 00:59:52 +0530

    Rover Movement

--- Sonar Rover/Sonar_Rover_Arduino/Sonar_Rover_Arduino.ino
+++ Sonar Rover/Sonar_Rover_Arduino/Sonar_Rover_Arduino.ino
@@ -39,3 +39,84 @@ void setup()
 
 void loop(){
 }
+
+
+void left()
+{
+  //analogWrite(en1, en1turn);
+  //analogWrite(en2, en2turn);
+  digitalWrite(en1, HIGH);
+  digitalWrite(en2, HIGH);  
+  digitalWrite(mlp, HIGH);
+  digitalWrite(mln, LOW);
+  digitalWrite(mrp, LOW);
+  digitalWrite(mrn, HIGH);
+  delay(leftdelay);
+  stop();
+  direc--;
+  travel = 0;
+  if (direc < 0)
+  {
+    direc = 3;
+  }
+  //Serial.println("LEFT TURN");
+}
+
+void right()
+{
+//  analogWrite(en1, en1turn);
+//  analogWrite(en2, en2turn);
+  digitalWrite(en1, HIGH);
+  digitalWrite(en2, HIGH);
+  digitalWrite(mlp, LOW);
+  digitalWrite(mln, HIGH);
+  digitalWrite(mrp, HIGH);
+  digitalWrite(mrn, LOW);
+  delay(rightdelay);
+  stop();
+  direc++;
+  travel = 0;
+  if (direc > 3)
+  {
+    direc = 0;
+  }
+  //Serial.println("RIGHT TURN");
+}
+
+void forward()
+{
+  digitalWrite(en1, HIGH);
+  digitalWrite(en2, HIGH);
+  digitalWrite(mlp, HIGH);
+  digitalWrite(mln, LOW);
+  digitalWrite(mrp, HIGH);
+  digitalWrite(mrn, LOW);
+  delay(forwdelay);
+  //stop();
+  travel = 10; //10cm
+  //Serial.println("FORWARD");
+}
+
+void backward()
+{
+  digitalWrite(en1, HIGH);
+  digitalWrite(en2, HIGH);
+  digitalWrite(mlp, LOW);
+  digitalWrite(mln, HIGH);
+  digitalWrite(mrp, LOW);
+  digitalWrite(mrn, HIGH);
+  delay(backdelay);
+  stop();
+  travel = 5; //10cm
+  //Serial.println("BACKWARD");
+}
+
+void stop()
+{
+  digitalWrite(en1, LOW);
+  digitalWrite(en2, LOW);
+  digitalWrite(mlp, LOW);
+  digitalWrite(mln, LOW);
+  digitalWrite(mrp, LOW);
+  digitalWrite(mrn, LOW);
+}
